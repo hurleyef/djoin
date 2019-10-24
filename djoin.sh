82be19af35104ae69d46c40c1ba8ebe675fc458d
hurleyef <hurleyef@gmail.com>
2019-10-24 00:04:52 -0400

    Update djoin.sh

--- djoin.sh
+++ djoin.sh
@@ -1,28 +1,18 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 
 #saner programming env: these switches turn some bugs into errors
 set -o errexit -o pipefail -o noclobber -o nounset
 
 
-#find and set echo command
-if [[ -f /usr/bin/echo ]]; then
-    ECHOCMD="/usr/bin/echo"
-elif [[ -f /bin/echo ]]; then
-    ECHOCMD="/bin/echo"
-else
-    exit 3
-fi
-
-
 #function to find commands
 find_command() {
     if [[ -f /usr/sbin/$1 ]]; then
-        $ECHOCMD "/usr/sbin/$1"
+        echo "/usr/sbin/$1"
     elif [[ -f /usr/bin/$1 ]]; then
-        $ECHOCMD "/usr/bin/$1"
+        echo "/usr/bin/$1"
     elif [[ -f /bin/$1 ]]; then
-        $ECHOCMD "/bin/$1"
+        echo "/bin/$1"
     else
             exit 1
     fi
@@ -30,21 +20,19 @@ find_command() {
 
 
 #find commands
-IPCMD=$(find_command ip) || exit $?
-GREPCMD=$(find_command grep) || exit $?
-SLEEPCMD=$(find_command sleep) || exit $?
-SYSTEMCTLCMD=$(find_command systemctl) || exit $?
-CHMODCMD=$(find_command chmod) || exit $?
-SEDCMD=$(find_command sed) || exit $?
-HOSTNAMECMD=$(find_command hostname) || exit $?
-UNAMECMD=$(find_command uname) || exit $?
-TRUE=$(find_command true) || exit $?
-FALSE=$(find_command false) || exit $?
+ipCmd=$(find_command ip) || exit $?
+grepCmd=$(find_command grep) || exit $?
+sleepCmd=$(find_command sleep) || exit $?
+systemctlCmd=$(find_command systemctl) || exit $?
+chmodCmd=$(find_command chmod) || exit $?
+sedCmd=$(find_command sed) || exit $?
+hostnameCmd=$(find_command hostname) || exit $?
+unameCmd=$(find_command uname) || exit $?
 
 
 #check for root
 if [[ $(/usr/bin/id -u) -ne 0 ]]; then
-   $ECHOCMD "This script must be run as root. Try: \"sudo $0\""
+   echo "This script must be run as root. Try: \"sudo $0\""
    exit 1
 fi
 
@@ -54,87 +42,87 @@ fi
 
 
 #enumerate options
-OPTIONS=hn:rfu:vd:a:ps:o:
-LONGOPTS=help,newname:,reboot,force,user:,verbose,domain:,authorized-group:,no-sudo-pass,sudo-group:,ou-path:
+options=hn:rfu:vd:a:ps:o:
+longOpts=help,newname:,reboot,force,user:,verbose,domain:,authorized-group:,no-sudo-pass,sudo-group:,ou-path:
 
 
 #test args
-PARSED=$(/usr/bin/getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@") || exit $?
+parsed=$(/usr/bin/getopt --options=$options --longoptions=$longOpts --name "$0" -- "$@") || exit $?
 
 
 #read getopt’s output this way to handle the quoting right:
-eval set -- "$PARSED"
+eval set -- "$parsed"
 
 
 #define default option values
-REBOOT=$FALSE
-FORCE=$FALSE
-VERBOSE=$FALSE
-NOPASS=$FALSE
-HELP=$FALSE
-DJOINACCOUNT=""
-OLDDOMAIN=$($HOSTNAMECMD -d)
-if [[ "$OLDDOMAIN" != "localdomain" ]]; then
-    DOMAIN=$OLDDOMAIN
+reboot=false
+force=false
+verbose=false
+noPass=false
+help=false
+djoinAccount=""
+oldDomain=$($hostnameCmd -d)
+if [[ "$oldDomain" != "localdomain" ]]; then
+    domain=$oldDomain
 else
-    DOMAIN=""
+    domain=""
 fi
-OLDHNAME=$($HOSTNAMECMD -s)
-if [[ "$OLDHNAME" != "localhost" ]]; then
-    HNAME=$OLDHNAME
+oldHostname=$($hostnameCmd -s)
+if [[ "$oldHostname" != "localhost" ]]; then
+    hostname=$oldHostname
 else
-    HNAME=""
+    hostname=""
 fi
-AUTHGROUP=""
-SUDOGROUP=""
-OUPATH=""
+authGroup=""
+sudoGroup=""
+ouPath=""
 
 
 #parse arguments
 while true; do
     case "$1" in
         -h|--help)
-            HELP=$TRUE
+            help=true
             break
             ;;
         -r|--reboot)
-            REBOOT=$TRUE
+            reboot=true
             shift
             ;;
         -f|--force)
-            FORCE=$TRUE
+            force=true
             shift
             ;;
         -v|--verbose)
-            VERBOSE=$TRUE
+            verbose=true
             shift
             ;;
         -u|--user)
-            DJOINACCOUNT="$2"
+            djoinAccount="$2"
             shift 2
             ;;
         -d|--domain)
-            DOMAIN="$2"
+            domain="$2"
             shift 2
             ;;
         -n|--newname)
-            HNAME="$2"
+            hostname="$2"
             shift 2
             ;;
         -a|--authorized-group)
-            AUTHGROUP="$2"
+            authGroup="$2"
             shift 2
             ;;
         -p|--no-sudo-pass)
-            NOPASS=$TRUE
+            noPass=true
             shift
             ;;
         -s|--sudo-group)
-            SUDOGROUP="$2"
+            sudoGroup="$2"
             shift 2
             ;;
         -o|--ou-path)
-            OUPATH="$2"
+            ouPath="$2"
             shift 2
             ;;
         --)
@@ -142,7 +130,7 @@ while true; do
             break
             ;;
         *)
-            $ECHOCMD "Programming error"
+            echo "Programming error"
             exit 3
             ;;
     esac
@@ -150,131 +138,131 @@ done
 
 
 #help
-if $HELP; then
-    $ECHOCMD "Usage: $0 [options]"
-    $ECHOCMD "Joins Linux system to an AD domain."
-    $ECHOCMD ""
-    $ECHOCMD "  -h, --help                        Print this help and exit successfully."
-    $ECHOCMD "  -n, --newname <hostname>          Renames host before joining to domain."
-    $ECHOCMD "  -r, --reboot                      Reboots host after domain join."
-    $ECHOCMD "  -f, --force                       Ignore system compatability checks. Only works on RHEL and Debian based systems."
-    $ECHOCMD "  -u, --user <username>             Specifies domain join user."
-    $ECHOCMD "  -v, --verbose                     Prints verbose output."
-    $ECHOCMD "  -d, --domain <domain>             Specifies domain to join."
-    $ECHOCMD "  -a, --authorized-group <group>    Specifies AD group allowed to login in to system. Default is to allow all groups."
-    $ECHOCMD "  -s, --sudo-group <group>          Specifies AD group to add to sudoers list."
-    $ECHOCMD "  -p, --no-sudo-pass                Allow sudo without a password for AD sudoers group."
-    $ECHOCMD "  -o, --ou-path <oupath>            Specifies OU path."
-    $ECHOCMD ""
-    $ECHOCMD "Report bugs to https://github.com/hurleyef/djoin/"
+if $help; then
+    echo "Usage: $0 [options]"
+    echo "Joins Linux system to an AD domain."
+    echo ""
+    echo "  -h, --help                        Print this help and exit successfully."
+    echo "  -n, --newname <hostname>          Renames host before joining to domain."
+    echo "  -r, --reboot                      Reboots host after domain join."
+    echo "  -f, --force                       Ignore system compatability checks. Only works on RHEL and Debian based systems."
+    echo "  -u, --user <username>             Specifies domain join user."
+    echo "  -v, --verbose                     Prints verbose output."
+    echo "  -d, --domain <domain>             Specifies domain to join."
+    echo "  -a, --authorized-group <group>    Specifies AD group allowed to login in to system. Default is to allow all groups."
+    echo "  -s, --sudo-group <group>          Specifies AD group to add to sudoers list."
+    echo "  -p, --no-sudo-pass                Allow sudo without a password for AD sudoers group."
+    echo "  -o, --ou-path <oupath>            Specifies OU path."
+    echo ""
+    echo "Report bugs to https://github.com/hurleyef/djoin/"
     exit 0
 fi
 
 
 #set verbose mode
-$VERBOSE && PIPETONULL=""
+$verbose && pipeToNull="" || pipeToNull='&>/dev/null'
 
 
 #detect OS
-if $FORCE; then
+if $force; then
     if [[ -f /etc/os-release ]]; then
         . /etc/os-release &>/dev/null
     else
-        ID=$($UNAMECMD -o)
-        VERSION_ID=$($UNAMECMD -r)
+        ID=$($unameCmd -o)
+        VERSION_ID=$($unameCmd -r)
     fi
     if [[ -f /usr/bin/yum ]]; then
-        DISTRO="EL"
+        distro="EL"
     elif [[ -f /usr/bin/apt ]]; then
-        DISTRO="DEB"
+        distro="DEB"
     else
-        $ECHOCMD "ERROR: System not compatible. Must be RHEL or Debian based."
+        echo "ERROR: System not compatible. Must be RHEL or Debian based."
         exit 1
     fi
 elif . /etc/os-release &>/dev/null; then
     if [[ "$ID" = "centos" ]] || [[ "$ID" = "fedora" ]] || [[ "$ID" = "rhel" ]]; then
-        DISTRO="EL"
+        distro="EL"
     elif [[ "$ID" = "debian" ]] || [[ "$ID" = "ubuntu" ]] || [[ "$ID" = "raspbian" ]]; then
-        DISTRO="DEB"
+        distro="DEB"
     fi
 else
-    $ECHOCMD "ERROR: System not compatible. Use --force to ignore this check."
+    echo "ERROR: System not compatible. Use --force to ignore this check."
     exit 1
 fi
 
 
 #wait for network, fail after 20 seconds
-NETWAIT=0
-until $IPCMD route | $GREPCMD default &>/dev/null; do
-    if [[ $NETWAIT -gt 20 ]]; then
-        $ECHOCMD "ERROR: No network detected."
+netWait=0
+until $ipCmd route | $grepCmd default &>/dev/null; do
+    if [[ $netWait -gt 20 ]]; then
+        echo "ERROR: No network detected."
         exit 1
     fi
-    $SLEEPCMD 1
-    NETWAIT=$((NETWAIT+1))
+    $sleepCmd 1
+    netWait=$((netWait+1))
 done
 
 
 #install vmware guest additions if applicable
-if [[ $(/usr/bin/systemd-detect-virt | $GREPCMD vmware) ]]; then
-    eval $ECHOCMD "VMWARE GUEST DETECTED, INSTALLING GUEST ADDITIONS" $PIPETONULL
-    if [[ "$DISTRO" = "EL" ]]; then
-        eval /usr/bin/yum install open-vm-tools -y $PIPETONULL
-        eval "$SYSTEMCTLCMD" enable --now vmtoolsd $PIPETONULL
-    elif [[ "$DISTRO" = "DEB" ]]; then
-        eval /usr/bin/apt install open-vm-tools -y $PIPETONULL
-        eval "$SYSTEMCTLCMD" enable --now open-vm-tools $PIPETONULL
+if [[ $(/usr/bin/systemd-detect-virt | $grepCmd vmware) ]]; then
+    eval echo "VMWARE GUEST DETECTED, INSTALLING GUEST ADDITIONS" $pipeToNull
+    if [[ "$distro" = "EL" ]]; then
+        eval /usr/bin/yum install open-vm-tools -y $pipeToNull
+        eval "$systemctlCmd" enable --now vmtoolsd $pipeToNull
+    elif [[ "$distro" = "DEB" ]]; then
+        eval /usr/bin/apt install open-vm-tools -y $pipeToNull
+        eval "$systemctlCmd" enable --now open-vm-tools $pipeToNull
     fi
 fi
 
 
 #install dependancies
-eval $ECHOCMD "INSTALLING DEPENDANCIES" $PIPETONULL
-if [[ "$DISTRO" = "EL" ]]; then
+eval echo "INSTALLING DEPENDANCIES" $pipeToNull
+if [[ "$distro" = "EL" ]]; then
     DEPS="realmd sssd adcli PackageKit sudo samba-common-tools oddjob oddjob-mkhomedir krb5-workstation bind-utils"
-    eval /usr/bin/yum update -y $PIPETONULL
-    eval /usr/bin/yum install -y "$DEPS" $PIPETONULL
-elif [[ "$DISTRO" = "DEB" ]]; then
+    eval /usr/bin/yum update -y $pipeToNull
+    eval /usr/bin/yum install -y "$DEPS" $pipeToNull
+elif [[ "$distro" = "DEB" ]]; then
     DEPS="realmd sssd adcli packagekit sudo samba-common sssd-tools samba-common-bin samba-libs krb5-user dnsutils"
     /usr/bin/apt-get update &>/dev/null
-    eval DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get upgrade -qq $PIPETONULL
-    eval DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get install -qq "$DEPS" $PIPETONULL
+    eval DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get upgrade -qq $pipeToNull
+    eval DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get install -qq "$DEPS" $pipeToNull
 fi
 
 
 #enable sssd
-[[ "$DISTRO" = "EL" ]] && $SYSTEMCTLCMD enable --now sssd.service &>/dev/null
+[[ "$distro" = "EL" ]] && $systemctlCmd enable --now sssd.service &>/dev/null || true
 
 
 #generate ssh keys
-eval $ECHOCMD "GENERATING NEW SSH HOST KEYS" $PIPETONULL
-eval /usr/bin/ssh-keygen -A $PIPETONULL
+eval echo "GENERATING NEW SSH HOST KEYS" $pipeToNull
+eval /usr/bin/ssh-keygen -A $pipeToNull
 
 
 #configure pam
-if [[ "$DISTRO" == "DEB" ]]; then
-    $ECHOCMD "session required pam_mkhomedir.so skel=/etc/skel/ umask=077" | /usr/bin/tee -a /etc/pam.d/common-session &>/dev/null
+if [[ "$distro" == "DEB" ]]; then
+    echo "session required pam_mkhomedir.so skel=/etc/skel/ umask=077" | /usr/bin/tee -a /etc/pam.d/common-session &>/dev/null
 fi
 
 
 #prompt for domain to join if not provided or parsed
-[[ "$HNAME" ]] || read -rp "New Hostname: " HNAME
+[[ "$hostname" ]] || read -rp "New Hostname: " hostname
 
 
 #prompt for domain to join if not provided or parsed
-[[ "$DOMAIN" ]] || read -rp "Domain: " DOMAIN
+[[ "$domain" ]] || read -rp "Domain: " domain
 
 
 #test domain connectivity
-eval $ECHOCMD "LOCATING DOMAIN CRONTROLLER FOR ${DOMAIN^^}" $PIPETONULL
-if ! eval /usr/bin/nslookup -type=SRV _ldap._tcp.dc._msdcs."$DOMAIN" $PIPETONULL; then
-    $ECHOCMD "ERROR: Cannot locate domain $DOMAIN."
+eval echo "LOCATING DOMAIN CRONTROLLER FOR ${domain^^}" $pipeToNull
+if ! eval /usr/bin/nslookup -type=SRV _ldap._tcp.dc._msdcs."$domain" $pipeToNull; then
+    echo "ERROR: Cannot locate domain $domain."
     exit 1
 fi
 
 
 #configure hostname
-/usr/bin/hostnamectl set-hostname "$HNAME"."$DOMAIN"
+/usr/bin/hostnamectl set-hostname "$hostname"."$domain"
 
 
 #configure kerberos
@@ -284,17 +272,17 @@ fi
  ticket_lifetime = 24h
  renew_lifetime = 7d
  forwardable = true
- default_realm = ${DOMAIN^^}
+ default_realm = ${domain^^}
  default_ccache_name = KEYRING:persistent:%{uid}
 
- default_realm = ${DOMAIN^^}
+ default_realm = ${domain^^}
 [realms]
- ${DOMAIN^^} = {
+ ${domain^^} = {
  }
 
-[$DOMAIN]
- $DOMAIN = ${DOMAIN^^}
- .$DOMAIN = ${DOMAIN^^}
+[$domain]
+ $domain = ${domain^^}
+ .$domain = ${domain^^}
 EOF
 
 
@@ -311,55 +299,55 @@ automatic-install = yes
 default-home = /home/%u
 default-shell = /bin/bash
 
-[$DOMAIN]
+[$domain]
 user-principal = yes
 fully-qualified-names = no
 EOF
 
 
 #prompt for domain join account if not already provided
-[[ "$DJOINACCOUNT" ]] || read -rp "Username: " DJOINACCOUNT
+[[ "$djoinAccount" ]] || read -rp "Username: " djoinAccount
 
 
 #define realm command arguments
-REALMARGS="$DOMAIN --user $DJOINACCOUNT --membership-software=adcli"
-if [[ "$OUPATH" ]]; then
-    OUPATH=${OUPATH^^}
-    if [[ "${OUPATH:0:3}" != "OU=" ]] && [[ "${OUPATH:0:3}" != "CN=" ]]; then
-        OUPATH="OU=$OUPATH"
+REALMARGS="$domain --user $djoinAccount --membership-software=adcli"
+if [[ "$ouPath" ]]; then
+    ouPath=${ouPath^^}
+    if [[ "${ouPath:0:3}" != "OU=" ]] && [[ "${ouPath:0:3}" != "CN=" ]]; then
+        ouPath="OU=$ouPath"
     fi
-    REALMARGS+=" --computer-ou=\"$OUPATH\""
+    REALMARGS+=" --computer-ou=\"$ouPath\""
 fi
 
 
 #join domain
-JOINCOUNTER=0
-until /usr/sbin/realm list | eval "$GREPCMD" "$DOMAIN" &>/dev/null; do
-    if [[ $JOINCOUNTER -gt 2 ]]; then
-        $ECHOCMD "ERROR: Authorization failure."
-        [[ "$OLDDOMAIN" ]] && OLDHNAME+=".$OLDDOMAIN"
-        /usr/bin/hostnamectl set-hostname "$OLDHNAME"
+joinCounter=0
+until /usr/sbin/realm list | eval "$grepCmd" "$domain" &>/dev/null; do
+    if [[ $joinCounter -gt 2 ]]; then
+        echo "ERROR: Authorization failure."
+        [[ "$oldDomain" ]] && oldHostname+=".$oldDomain"
+        /usr/bin/hostnamectl set-hostname "$oldHostname"
         exit 1
     fi
     #prompt for password
     read -srp "Password: " DJOINPASSWORD
-    $ECHOCMD ""
-    $ECHOCMD "$DJOINPASSWORD" | eval /usr/sbin/realm join "$REALMARGS" &>/dev/null || $TRUE
-    JOINCOUNTER=$((JOINCOUNTER+1))
+    echo ""
+    echo "$DJOINPASSWORD" | eval /usr/sbin/realm join "$REALMARGS" &>/dev/null || true
+    joinCounter=$((joinCounter+1))
 done
 
 
 #configure sssd
 /usr/bin/tee /etc/sssd/sssd.conf 1>/dev/null << EOF
 [sssd]
-domains = $DOMAIN
+domains = $domain
 config_file_version = 2
 services = nss, pam
 
-[domain/$DOMAIN]
-ad_domain = $DOMAIN
-krb5_realm = ${DOMAIN^^}
-#default_domain_suffix = $DOMAIN
+[domain/$domain]
+ad_domain = $domain
+krb5_realm = ${domain^^}
+#default_domain_suffix = $domain
 use_fully_qualified_names = false
 
 re_expression = (((?P<domain>[^\\\]+)\\\(?P<name>.+$))|((?P<name>[^@]+)@(?P<domain>.+$))|(^(?P<name>[^@\\\]+)$))
@@ -381,59 +369,59 @@ dyndns_update_ptr = true
 dyndns_ttl = 3600
 ldap_id_mapping = true
 EOF
-$CHMODCMD 600 /etc/sssd/sssd.conf
-$SYSTEMCTLCMD restart sssd.service
+$chmodCmd 600 /etc/sssd/sssd.conf
+$systemctlCmd restart sssd.service
 
 
 #configure authorization
-if [[ "$AUTHGROUP" ]] && [[ ! $(/usr/sbin/realm list | $GREPCMD "permitted-groups.*$AUTHGROUP")  ]]; then
-    /usr/sbin/realm permit --groups "$AUTHGROUP"
+if [[ "$authGroup" ]] && [[ ! $(/usr/sbin/realm list | $grepCmd "permitted-groups.*$authGroup")  ]]; then
+    /usr/sbin/realm permit --groups "$authGroup"
 fi
 
 
 #print realm status
-eval /usr/sbin/realm list $PIPETONULL
+eval /usr/sbin/realm list $pipeToNull
 
 
 #configure sudo
-if [[ "$SUDOGROUP" ]]; then
+if [[ "$sudoGroup" ]]; then
     #escape spaces in group name
-    SUDOGROUP="$($SEDCMD "s/ /\\\\\ /g" <<< "$SUDOGROUP")"
+    sudoGroup="$($sedCmd "s/ /\\\\\ /g" <<< "$sudoGroup")"
     #remove any preexisting authorization for group
-    < /etc/sudoers $SEDCMD "/%$($SEDCMD 's/\\/\\\\/g' <<< "$SUDOGROUP")/Id" | EDITOR="/usr/bin/tee" /usr/sbin/visudo &>/dev/null
+    < /etc/sudoers $sedCmd "/%$($sedCmd 's/\\/\\\\/g' <<< "$sudoGroup")/Id" | EDITOR="/usr/bin/tee" /usr/sbin/visudo &>/dev/null
     #authorize group
-    if $NOPASS; then
-        $ECHOCMD "%$SUDOGROUP    ALL=(ALL)    NOPASSWD:    ALL" | EDITOR="/usr/bin/tee -a" /usr/sbin/visudo &>/dev/null
+    if $noPass; then
+        echo "%$sudoGroup    ALL=(ALL)    NOPASSWD:    ALL" | EDITOR="/usr/bin/tee -a" /usr/sbin/visudo &>/dev/null
     else
-        $ECHOCMD "%$SUDOGROUP    ALL=(ALL)    ALL" | EDITOR="/usr/bin/tee -a" /usr/sbin/visudo &>/dev/null
+        echo "%$sudoGroup    ALL=(ALL)    ALL" | EDITOR="/usr/bin/tee -a" /usr/sbin/visudo &>/dev/null
     fi
 fi
 
 
 #configure ssh to use gssapi and disable root login
-$SEDCMD -i "s/$($GREPCMD "GSSAPIAuthentication" < /etc/ssh/sshd_config)/GSSAPIAuthentication yes/g" /etc/ssh/sshd_config
-$SEDCMD -i "s/$($GREPCMD "GSSAPICleanupCredentials" < /etc/ssh/sshd_config)/GSSAPICleanupCredentials yes/g" /etc/ssh/sshd_config
-$SEDCMD -i "s/$($GREPCMD "PermitRootLogin [yn]" < /etc/ssh/sshd_config)/PermitRootLogin no/g" /etc/ssh/sshd_config
+$sedCmd -i "s/$($grepCmd "GSSAPIAuthentication" < /etc/ssh/sshd_config)/GSSAPIAuthentication yes/g" /etc/ssh/sshd_config
+$sedCmd -i "s/$($grepCmd "GSSAPICleanupCredentials" < /etc/ssh/sshd_config)/GSSAPICleanupCredentials yes/g" /etc/ssh/sshd_config
+$sedCmd -i "s/$($grepCmd "PermitRootLogin [yn]" < /etc/ssh/sshd_config)/PermitRootLogin no/g" /etc/ssh/sshd_config
 
 
 #restart ssh
-$SYSTEMCTLCMD restart sshd.service
+$systemctlCmd restart sshd.service
 
 
 #purge user kerberos tickets on logout
 /usr/bin/touch /etc/bash.bash_logout
-if [[ ! $($GREPCMD kdestroy < /etc/bash.bash_logout) ]]; then
-    $ECHOCMD kdestroy | /usr/bin/tee /etc/bash.bash_logout &>/dev/null
+if [[ ! $($grepCmd kdestroy < /etc/bash.bash_logout) ]]; then
+    echo kdestroy | /usr/bin/tee /etc/bash.bash_logout &>/dev/null
 fi
 
 
 #remove domain join cronjob and delete script
-#/usr/bin/crontab -l | eval $GREPCMD -v 'djoin.sh'  | /usr/bin/crontab - || $TRUE
+#/usr/bin/crontab -l | eval $grepCmd -v $0  | /usr/bin/crontab - || true
 #/usr/bin/rm -- "$0"
 
 
 #reboot system
-$REBOOT && $SYSTEMCTLCMD reboot
+$reboot && $systemctlCmd reboot
 
 
 #fin
